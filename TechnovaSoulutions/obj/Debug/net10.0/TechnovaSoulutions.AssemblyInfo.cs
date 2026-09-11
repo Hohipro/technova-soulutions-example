46bd8bb909dfbd7fc0561d06aebf298b9c056ef8
Hohipro <moritz.scheffler@outlook.com>
2026-09-11 14:54:48 +0200

    random changes i cant get rid off

--- TechnovaSoulutions/obj/Debug/net10.0/TechnovaSoulutions.AssemblyInfo.cs
+++ TechnovaSoulutions/obj/Debug/net10.0/TechnovaSoulutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechnovaSoulutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0351c28b9f1954cb9c406c1249be529768bb7133")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechnovaSoulutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechnovaSoulutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
